358946ffda4ee0005840e33a15dd856721739949
ammmoniac <danielyanarmine98@gmail.com>
2023-05-01 22:22:13 +0400

    added material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 import { AngularFireModule } from "@angular/fire/compat";
 import { AngularFireDatabaseModule } from "@angular/fire/compat/database";
 
@@ -10,12 +12,14 @@ import { PagesListComponent } from './pages-list/pages-list.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AfService } from "./providers/af.service";
 import { AngularFireAuthModule } from "@angular/fire/compat/auth";
-import { AngularFirestore, AngularFirestoreModule } from "@angular/fire/compat/firestore";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AdminPageComponent } from './admin-page/admin-page.component';
 import { AdminGuard } from "./guards/admin.guard";
 import { SubscriberGuard } from "./guards/subscriber.guard";
+
+import { MaterialModule } from "./material.module";
 
 @NgModule({
   declarations: [
@@ -32,7 +36,8 @@ import { SubscriberGuard } from "./guards/subscriber.guard";
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    MaterialModule
   ],
   providers: [AfService, AdminGuard, SubscriberGuard],
   bootstrap: [AppComponent]
