deff00e5b76b87109d662a6cbb384a7bab7e69f6
ammmoniac <danielyanarmine98@gmail.com>
2023-05-02 19:06:33 +0400

    added posts service

--- src/app/service/posts/posts.service.ts
+++ src/app/service/posts/posts.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs';
+export interface Post {
+  title: "";
+  url: "";
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  getPosts() {
+    return this.afs.collection('posts').snapshotChanges().pipe(
+      map(post => {
+        return post.map(a => {
+          const data = a.payload.doc.data() as Post;
+          const id = a.payload.doc.id;
+          return {id, ...data};
+        })
+      })
+    )
+  }
+
+  addPost(post: Post) {
+    this.afs.collection('posts').add(post);
+  }
+
+  deletePost(postId: string) {
+    this.afs.doc('posts/' + postId).delete();
+  }
+
+  updatePost(postId: string, post: Post) {
+    this.afs.doc('posts/' + postId).update(post);
+  }
 }
